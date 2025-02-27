612582f3908db344429834c93e8f9d1fcb450c78
pepo993 <peppepola@hotmail.it>
2025-02-27 22:46:20 +0100

    Update config.py

--- config.py
+++ config.py
@@ -9,14 +9,14 @@ load_dotenv()
 # Token del bot Telegram
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 
-# URL della Web App Telegram (SOSTITUISCI con il tuo link!)
+# URL della Web App Telegram
 WEBAPP_URL = "https://telegram-bingo-bot.vercel.app"
 
 
 
 # Impostazioni finanziarie
 CURRENCY = "TON"  # Puoi modificarlo in base al token usato
-COSTO_CARTELLA = 1  # Prezzo di una cartella
+COSTO_CARTELLA = 0.2  # Prezzo di una cartella
 FEE_PERCENTUALE = 0.10  # Percentuale trattenuta dal sistema (10%)
 JACKPOT_BINGO = 0.90  # Percentuale destinata al vincitore del Bingo (90%)
 JACKPOT_CINQUINA = 0.10  # Percentuale destinata al vincitore della Cinquina (10%)
