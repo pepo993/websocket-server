af56cb89ef8f326caf8b23037147687c15d1ee7a
pepo993 <peppepola@hotmail.it>
2025-02-28 13:53:19 +0100

    Update websocket_server.py

--- websocket_server.py
+++ websocket_server.py
@@ -94,7 +94,8 @@ app.router.add_get('/health', health_check)
 
 async def start_server():
     # Creiamo il server WebSocket con il parametro `path` corretto
-    websocket_server = await websockets.serve(handler, "0.0.0.0", PORT, ping_interval=None, ping_timeout=None)
+    websocket_server = await websockets.serve(handler, "0.0.0.0", PORT, path="/ws", ping_interval=None, ping_timeout=None)
+
     print(f"WebSocket Server avviato su ws://0.0.0.0:{PORT}/ws")
 
     # Avvia il server HTTP per l'health check
