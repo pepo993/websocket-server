dfb3b9316f0e45d601973e78a0eb8fdd3311f557
pepo993 <peppepola@hotmail.it>
2025-02-28 14:24:11 +0100

    Update websocket_server.py

--- websocket_server.py
+++ websocket_server.py
@@ -5,6 +5,9 @@ import os
 from aiohttp import web
 from game_logic import load_game_data
 
+# Assicura che la cartella "data/" esista
+os.makedirs("data", exist_ok=True)
+
 # Ottieni la porta assegnata da Railway
 PORT = int(os.getenv("PORT", 8002))
 
